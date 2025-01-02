c2ace8ca2e55d5584fb46816c1ed0e1afa104518
vaishnavichavare04 <vaishnavichavare16@gmail.com>
2025-01-02 20:40:13 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css'
+
+
+const Navbar = () =>{
+    return(
+        <>
+        <nav>
+          <img src='kitty-cat.webp' alt='logo' />
+
+          <ul>
+            <li>Home</li>
+            <li>Explore</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
